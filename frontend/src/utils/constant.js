cf45335cab5eb58a7de7ef49394f1aa0ea67e2d8
vinay1945 <vinaykumarporandla19@gmail.com>
2025-08-03 23:14:03 +0530

    add backend live end points

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const TWEET_API_END_POINT = "http://localhost:8000/api/v1/tweet";
+export const USER_API_END_POINT = "https://twitter-clone-3okn.onrender.com/api/v1/user";
+export const TWEET_API_END_POINT = "https://twitter-clone-3okn.onrender.com/api/v1/tweet";
